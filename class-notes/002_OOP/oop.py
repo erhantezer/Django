0632bb1e03e5ac85beb1b0b22b5def2811904cae
Erhan <erhantezer68@gmail.com>
2022-08-16 20:25:32 +0300

    updated

--- class-notes/002_OOP/oop.py
+++ class-notes/002_OOP/oop.py
@@ -1,11 +1,19 @@
 import os
 os.system('cls' if os.name == 'nt' else 'clear')
 
+print("------------------------------------------------")
+def print_types(data):
+    for i in data:
+        print(i, type(i))
+        
+test = [122, "victor", [1,2,3], (1,2,3), True, lambda x:x]
+print_types(test)   
 
 
 
+  
 
-
+print("------------------------------------------------")
 
 
 

bcc13fc6449263a6b7b339d5aee102ec900c934f
Erhan <erhantezer68@gmail.com>
2022-08-31 21:27:41 +0300

    updated

--- class-notes/012_DJ_Auth_1/user_example/urls.py
+++ class-notes/012_DJ_Auth_1/user_example/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from .views import (
     home,
+    register,
     special
 )
 
 urlpatterns = [
     path('', home, name="home"),
     path('special', special, name="special"),
+    path('register', register, name="register"),
 ]

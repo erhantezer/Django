633729c3e5cd3010f735d908ee750e206977afba
Erhan <erhantezer68@gmail.com>
2022-09-01 18:03:12 +0300

    updated

--- class-notes/012_DJ_Auth_1/main/urls.py
+++ class-notes/012_DJ_Auth_1/main/urls.py
@@ -17,7 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    
     path('admin/', admin.site.urls),
     path('', include('user_example.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    
 ]

73638b5abf67748a2c7ffa525025987fb44e1ced
Erhan <erhantezer68@gmail.com>
2022-09-16 00:07:21 +0300

    updated

--- calısma/todo-crud/todo/views.py
+++ calısma/todo-crud/todo/views.py
@@ -1,5 +1,14 @@
+from .forms import TodoForm
+from todo.models import Todo
 from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, "todo/home.html")
+    todos = Todo.objects.all()
+    form = TodoForm()
+    
+    context = {
+        "todos":todos,
+        "form":form,
+    }
+    return render(request, "todo/home.html", context)

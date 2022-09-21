717c97d901f270c3232fd39b6010806bb206678f
Erhan <erhantezer68@gmail.com>
2022-09-21 18:22:31 +0300

    updated

--- calısma/AUTH-1/user_example/urls.py
+++ calısma/AUTH-1/user_example/urls.py
@@ -1,7 +1,16 @@
 
-from .views import home
+from .views import (
+home, 
+register, 
+special
+)
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path("", home, name="home")
+    path("", home, name="home"),
+    path('special/', special, name="special"),
+    path("register/", register, name="register"),
+    path('password_change/', auth_views.PasswordChangeView.as_view(
+        template_name="registration/password_change.html"), name="password_change")
 ]
--- calısma/AUTH-1/user_example/views.py
+++ calısma/AUTH-1/user_example/views.py
@@ -1,5 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+#! Create your views here.
 def home(request):
     return render(request, "user_example/home.html")
+
+
+
+@login_required
+def special(request):
+    return render(request, "user_example/special.html")
+
+
+
+def register(request):
+    form = UserCreationForm(request.POST or None)
+    # form = UserCreationForm()
+    # if request.method == 'POST':
+    #     form = UserCreationForm(request.POST)
+    if form.is_valid():
+        form.save()
+
+        # username = form.cleaned_data.get("username")
+        # password = form.cleaned_data.get("password2")
+
+        # user = authenticate(username=username, password=password)
+
+        # login(request, user)
+
+        return redirect("login")
+
+    context = {
+        'form': form
+    }
+    return render(request, 'registration/register.html', context)

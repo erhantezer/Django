a4581c07d4ca93ee74cb9eef567c06595107f080
Erhan <erhantezer68@gmail.com>
2022-09-25 10:57:43 +0300

    updated

--- calısma/blogapp-django/blog/models.py
+++ calısma/blogapp-django/blog/models.py
@@ -56,4 +56,9 @@ class Post(models.Model):
             return self.user.username
         
     class PostView(models.Model):
-        pass
+        user = models.ForeignKey(User, on_delete=models.CASCADE)
+        post = models.ForeignKey(Post, on_delete=models.CASCADE)
+        time_stamp = models.DateTimeField(auto_now_add=True)
+        
+        def __str__(self):
+            return self.user.username

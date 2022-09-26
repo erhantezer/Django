64fdb2bbc9aa26f859e16d089abb73384339c645
Erhan <erhantezer68@gmail.com>
2022-09-26 17:01:00 +0300

    updated

--- calısma/weather_app/weather/views.py
+++ calısma/weather_app/weather/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    url = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}"
     return render(request, "weather/index.html")

92099c566b7ca60bd5055c0041f382c0db2bc160
Erhan <erhantezer68@gmail.com>
2022-10-02 01:22:28 +0300

    updated

--- calısma/Admin-panel/product/admin.py
+++ calısma/Admin-panel/product/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Product
+from django.utils import timezone 
 
 # Register your models here.
 admin.site.register(Product)
@@ -12,7 +13,7 @@ admin.site.index_title = "Welcome to Erhan Admin Portal"
 
 #? admin i istediğimiz gibi düzenliyoruz
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("name", "create_date", "update_date")
+    list_display = ("name", "create_date", "is_in_stock" "update_date", "added_days_ago")
     list_editable = ("is_in_stock",)
     list_filter = ("is_in_stock", "create_date")
     ordering = ("name",)  
@@ -20,4 +21,36 @@ class ProductAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug' : ('name',)}   #? when adding product in admin site example: erhan-tezer
     list_per_page = 25
     date_hierarchy = "update_date"
+    # inlines = (ReviewInline,)
+    # readonly_fields = ("bring_image",)
     # fields = (('name', 'slug'), 'description', "is_in_stock") #? fieldset kullandığımız zaman bunu kullanamayız
+    fieldsets = (
+        (None, {
+            "fields": (
+                ('name', 'slug'), "is_in_stock" # to display multiple fields on the same line, wrap those fields in their own tuple
+            ),
+            # 'classes': ('wide', 'extrapretty'), wide or collapse
+        }),
+        ('Optionals Settings', {
+            "classes" : ("collapse", ),
+            "fields" : ("description",),
+            'description' : "You can use this section for optionals settings"
+        })
+    )
+    
+
+
+
+    actions = ("is_in_stock", )
+
+    def is_in_stock(self, request, queryset):
+            count = queryset.update(is_in_stock=True)
+            self.message_user(request, f"{count} çeşit ürün stoğa eklendi")
+            
+    is_in_stock.short_description = 'İşaretlenen ürünleri stoğa ekle'
+    
+    
+	
+    def added_days_ago(self, product):
+        fark = timezone.now() - product.create_date
+        return fark.days

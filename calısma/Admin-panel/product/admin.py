b7193e789d4880d1a1ca741ceabaede93eafa38d
Erhan <erhantezer68@gmail.com>
2022-10-02 00:45:20 +0300

    updated

--- calısma/Admin-panel/product/admin.py
+++ calısma/Admin-panel/product/admin.py
@@ -10,6 +10,8 @@ admin.site.site_title = "ERHAN PROJECT" #? html deki title gibi
 admin.site.site_header = "Erhan Admin Portal"
 admin.site.index_title = "Welcome to Erhan Admin Portal"
 
-
+#? admin i istediğimiz gibi düzenliyoruz
 class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "create_date", "update_date")
+    list_editable = ("is_in_stock",)
     

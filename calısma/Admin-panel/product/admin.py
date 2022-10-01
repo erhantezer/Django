663d775698e038070a19e9dcb79a7a25216c421f
Erhan <erhantezer68@gmail.com>
2022-10-02 00:52:20 +0300

    updated

--- calısma/Admin-panel/product/admin.py
+++ calısma/Admin-panel/product/admin.py
@@ -14,4 +14,10 @@ admin.site.index_title = "Welcome to Erhan Admin Portal"
 class ProductAdmin(admin.ModelAdmin):
     list_display = ("name", "create_date", "update_date")
     list_editable = ("is_in_stock",)
-    
+    list_filter = ("is_in_stock", "create_date")
+    ordering = ("name",)  
+    search_fields = ("name",)
+    prepopulated_fields = {'slug' : ('name',)}   #? when adding product in admin site example: erhan-tezer
+    list_per_page = 25
+    date_hierarchy = "update_date"
+    # fields = (('name', 'slug'), 'description', "is_in_stock") #? fieldset kullandığımız zaman bunu kullanamayız
